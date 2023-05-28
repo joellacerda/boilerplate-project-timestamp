08403423c76daba519cd77339b254633bf64d2f5
Joel Lacerda <joellacerdaol@gmail.com>
2023-05-28 11:26:44 -0300

    final commit

--- index.js
+++ index.js
@@ -2,31 +2,46 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
+var moment = require("moment");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
-
+app.get("/api/:time", function (req, res) {
+  let time = req.params.time;
+  if (moment(time, "YYYY-MM-DD", true).isValid()) {
+    res.json({
+      unix: moment(time).valueOf(),
+      utc: moment(time).utc().format("ddd, DD MMM YYYY HH:mm:ss [GMT]"),
+    });
+  } else if (moment(time, "X", true).isValid()) {
+    res.json({
+      unix: time,
+      utc: moment.unix(time).utc().format("ddd, DD MMM YYYY HH:mm:ss [GMT]"),
+    });
+  } else {
+    res.json({ error: "Invalid Date" });
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
